03195599b7a8566c76f296484327185647c6ae9e
Ethan Grebmeier <ethangrebmeier@gmail.com>
2021-06-02 11:05:03 -0700

    Added boolean to distinguish placeholder items from user created items

--- redux/applets/checklist/getItems.js
+++ redux/applets/checklist/getItems.js
@@ -6,47 +6,54 @@ const getItems = () => {
                 title: 'Eng Reading',
                 date: 'May 22, 2021 03:24:00',
                 isCompleted: false,
-                isChecked: false
+                isChecked: false,
+                isPlaceholder: true
             },
             {
                 id: 132346, 
                 title: 'Stats Homework',
                 date: 'May 20, 2021 03:24:00',
                 isCompleted: false,
-                isChecked: false
+                isChecked: false,
+                isPlaceholder: true
             },
             {
                 id: 132343, 
                 title: 'Pay Bills',
                 isCompleted: false,
-                isChecked: false
+                isChecked: false,
+                isPlaceholder: true
             },
             {
                 id: 134345, 
-                title: 'Eng Reading',
-                date: 'May 22, 2021 03:24:00',
+                title: 'Pay Rent',
+                date: 'May 1, 2021 03:24:00',
                 isCompleted: false,
-                isChecked: false
+                isChecked: false,
+                isPlaceholder: true
             },
             {
                 id: 132146, 
-                title: 'Stats Homework',
-                date: 'May 20, 2021 03:24:00',
+                title: '5 Job Applications',
+                date: 'June 20, 2021 03:24:00',
                 isCompleted: false,
-                isChecked: false
+                isChecked: false,
+                isPlaceholder: true
             },         
             {
                 id: 123532, 
                 title: 'UI Mockups',
                 isCompleted: true,
-                isChecked: true
+                isChecked: true,
+                isPlaceholder: true
             },
             {
                 id: 123435,
                 title: 'Data Structure Planning',
                 date: 'May 22, 2021 03:24:00',
                 isCompleted: true,
-                isChecked: true
+                isChecked: true,
+                isPlaceholder: true
             }
         ]
 }
