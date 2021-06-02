36e8291e1c01d291286dca27c84f8bfdee83534e
Ethan Grebmeier <ethangrebmeier@gmail.com>
2021-06-02 10:09:04 -0700

    Created method to fetch items from endpoint. Currently has dummy data

---
+++ redux/applets/checklist/getItems.js
@@ -0,0 +1,54 @@
+const getItems = () => {
+    //TODO: Axios logic
+    return [
+            {
+                id: 132345, 
+                title: 'Eng Reading',
+                date: 'May 22, 2021 03:24:00',
+                isCompleted: false,
+                isChecked: false
+            },
+            {
+                id: 132346, 
+                title: 'Stats Homework',
+                date: 'May 20, 2021 03:24:00',
+                isCompleted: false,
+                isChecked: false
+            },
+            {
+                id: 132343, 
+                title: 'Pay Bills',
+                isCompleted: false,
+                isChecked: false
+            },
+            {
+                id: 134345, 
+                title: 'Eng Reading',
+                date: 'May 22, 2021 03:24:00',
+                isCompleted: false,
+                isChecked: false
+            },
+            {
+                id: 132146, 
+                title: 'Stats Homework',
+                date: 'May 20, 2021 03:24:00',
+                isCompleted: false,
+                isChecked: false
+            },         
+            {
+                id: 123532, 
+                title: 'UI Mockups',
+                isCompleted: true,
+                isChecked: true
+            },
+            {
+                id: 123435,
+                title: 'Data Structure Planning',
+                date: 'May 22, 2021 03:24:00',
+                isCompleted: true,
+                isChecked: true
+            }
+        ]
+}
+
+export default getItems
