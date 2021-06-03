c286abb31b0e5d2723f4d83bef705fc17272ec14
Ethan Grebmeier <ethangrebmeier@gmail.com>
2021-06-02 17:23:24 -0700

    Added thunk middleware

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore} from '@reduxjs/toolkit'
+import thunk from 'redux-thunk'
 import checklistReducer from './applets/checklist/checklistSlice'
 
 export default configureStore({
   reducer: {
-    checklist: checklistReducer
-  },
+    checklist: checklistReducer,
+    middleware: [thunk]
+  }
 })
