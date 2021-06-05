055e33279c3f12a6ea04c3d4d6b9559cb90464b9
Ethan Grebmeier <ethangrebmeier@gmail.com>
2021-06-04 22:20:06 -0700

    Added new colors and primary/secondary options to buttons

--- components/Buttons/Button.js
+++ components/Buttons/Button.js
@@ -1,16 +1,24 @@
 import styled from 'styled-components'
 
+
 const Button = styled.button`
     width: ${props => props.width};
     height: ${props => props.height};
-    padding: 5px;
-    background: none;
-    border: 2px solid black;
-    border-radius: 10px;
+    padding: 5px 10px 5px 10px;
+    background: ${props => !props.secondary ? 'rgba(255, 191, 0, 1)' : 'rgba(239, 206, 250, 1)'};
+    border: 3px solid black;
+    border-radius: 16px;
     font-family: 'Quicksand';
-    font-weight: 600;
+    font-weight: 900;
     font-size: 18px;
     cursor: pointer;
+    
+    && {
+        transition: none;
+    }
+    :hover{
+        background: ${props => !props.secondary ? 'rgba(255, 191, 0, .6)' : 'rgba(239, 206, 250, .6)'};
+    }
 `
 
 export default Button
