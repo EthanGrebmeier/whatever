8fa7078dcdd631297bf27ef052697cdda6d653f3
Ethan Grebmeier <ethangrebmeier@gmail.com>
2021-06-04 22:24:45 -0700

    Added notepad reducer

---
+++ redux/applets/notepad/notepadSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const notepadSlice = createSlice({
+  name: 'notepad',
+  initialState: {
+    title: '',
+    text: '',
+    loading: true,
+    error: null
+  },
+  reducers: {
+    fetchNotepadSuccess: (state, action) => {
+      return {
+        ...state,
+        loading: false,
+        title: action.payload.title,
+        text: action.payload.text
+      }
+    },
+    updateNotes: (state, action) => {
+      console.log(action.payload)
+      return {
+        ...state,
+        ...action.payload
+      }
+    },
+  }
+})
+
+// Action creators are generated for each case reducer function
+export const { 
+  fetchNotepadSuccess,
+  updateNotes
+} = notepadSlice.actions
+
+export default notepadSlice.reducer
--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore} from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import checklistReducer from './applets/checklist/checklistSlice'
+import notepadReducer from './applets/notepad/notepadSlice'
 
 export default configureStore({
   reducer: {
     checklist: checklistReducer,
+    notepad: notepadReducer,
     middleware: [thunk]
   }
 })
