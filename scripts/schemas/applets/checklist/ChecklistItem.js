04701f4fb36da0af4b6031d8b595023abc092b9c
Ethan Grebmeier <ethangrebmeier@gmail.com>
2021-06-04 22:25:06 -0700

    Created checklist item schema

---
+++ scripts/schemas/applets/checklist/ChecklistItem.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const ChecklistItemSchema = new mongoose.Schema({
+    title: String,
+    date: String, 
+    isCompleted: Boolean,
+    isChecked: Boolean
+}, {strict: false})
+
+
+module.exports = ChecklistItemSchema
