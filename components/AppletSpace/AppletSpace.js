809b9bb1729755b22d84655f56c27d1f94e998a6
Ethan Grebmeier <ethangrebmeier@gmail.com>
2021-06-04 22:19:44 -0700

    Added check for mobile in appletspace

--- components/AppletSpace/AppletSpace.js
+++ components/AppletSpace/AppletSpace.js
@@ -1,48 +1,20 @@
-import styled from 'styled-components'
-import Applet from '../Applets/Applet'
-import {getComponent} from '../../applets/applets'
+import DesktopAppletSpace from "./DesktopAppletSpace"
+import MobileAppletSpace from "./MobileAppletSpace"
 
-const Wrapper = styled.div`
-    width: 100%;
-    height: 85vh; 
-    position: relative;
-`     
 
-const AppletSpace = (props) => { 
-    
-    const setPosition = (oldPosition, newPosition) => {
-        let layout = {...props.layout}
-        for (let applet in layout.applets){
-            if (layout.applets[applet].position == oldPosition){
-                layout.applets[applet].position = newPosition
-            }
-        }
-        props.setLayout(layout)
+const AppletSpace = (props) => {
+    if (props.isMobile){
+        return (
+        <MobileAppletSpace
+            {...props}
+        />)
     }
-
-
-    return(
-        <Wrapper>
-            {props.layout.applets && props.layout.applets.map( (applet, index) => {
-                return <Applet
-                    key={applet.id}
-                    applet={applet}
-                    closeApplet={props.closeApplet}
-                    applets={props.layout.applets}
-                    setPosition={setPosition}
-                    width={applet.width}
-                    height={applet.height}
-                    setWidth={props.setWidth}
-                    setHeight={props.setHeight}
-                    moveApplet={props.moveApplet}
-                    setLayout={props.setLayout}
-                    layout={props.layout}
-                >
-                    {getComponent(applet.id.slice(0, -3), {...props, applet})}
-                </Applet>
-            })}
-        </Wrapper>
+    
+    return ( 
+        <DesktopAppletSpace
+            {...props}
+        />
     )
 }
 
-export default AppletSpace 
+export default AppletSpace
