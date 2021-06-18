203d574ac6834fe239a54770d8471e408cb252dd
Ethan Grebmeier <ethangrebmeier@gmail.com>
2021-06-18 12:55:24 -0700

    Added logic to fetch the current component

--- components/AppletSpace/MobileAppletSpace.js
+++ components/AppletSpace/MobileAppletSpace.js
@@ -1,32 +1,35 @@
 import styled from 'styled-components'
+import { getMobileComponent } from '../../applets/applets'
+import MobileMenu from '../../applets/Mobile/MobileMenu/MobileMenu'
 
 const Wrapper = styled.div`
     width: 100%;
     height: 85vh; 
     position: relative;
+    padding-bottom: 10px;
 `     
 
-const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    background: #F7934C;
-    border-radius: 10px;
-    border: 2px solid black;
-    padding: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-`
-
-const MobileAppletSpace = () => {
+const MobileAppletSpace = ({mobileMenuOpen, setMobileMenuOpen, mobileAppletId, setMobileAppletId, ...rest}) => {
     return (
         <Wrapper>
-            <Container>
-                <h1>
-                    Whatever does not yet support mobile devices. Please check back soon, or visit us on desktop!
-                </h1>
-            </Container>
+            {getMobileComponent(mobileAppletId, {
+                mobileAppletId, 
+                setMobileAppletId, 
+                applet: {
+                    width: '100%',
+                    height: '100%',
+                },
+                ...rest
+            })}
+            {
+                mobileMenuOpen && (
+                    <MobileMenu
+                        setMobileAppletId={setMobileAppletId}
+                        mobileAppletId={mobileAppletId}
+                        setMobileMenuOpen={setMobileMenuOpen}
+                    />
+                )
+            }
         </Wrapper>
     )
 }
