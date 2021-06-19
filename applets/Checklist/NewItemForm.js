4addefb123c65ee9749db9723c5f44ca2c50a412
Ethan  Grebmeier <ethangrebmeier@gmail.com>
2021-06-19 13:30:48 -0700

    Changed placeholder text color

--- applets/Checklist/NewItemForm.js
+++ applets/Checklist/NewItemForm.js
@@ -10,6 +10,10 @@ const NewItemFormWrapper = styled.form`
     display: flex;
     justify-content: space-between;
     padding: 5px 0 5px 0;
+
+    & input::placeholder{
+        color: #f8f8f8;
+    }
 `
 
 const NewItemForm = ({inputItemTitle, setInputItemTitle, setInputItemDate,inputItemDate, submitForm, toggleForm, isWide}) => {
@@ -40,14 +44,18 @@ const NewItemForm = ({inputItemTitle, setInputItemTitle, setInputItemDate,inputI
                     onChange={(e) => setInputItemTitle(e.target.value) }
                     width='40%'
                     label='Item Title'
+                    height='100%'
                     autoFocus
+                    placeholder='Name'
                 />
                 <Input
                     type='date'
                     value={inputItemDate}
                     onChange={(e) => setInputItemDate(e.target.value) }
                     width='37%'
+                    height='100%'
                     label='Item Date'
+                    placeholder='Date'
                 />
             </Section>
             <Section 
