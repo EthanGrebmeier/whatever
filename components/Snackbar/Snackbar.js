ce66a42ef3793394753ca26bf72393ca0a858b0d
Ethan  Grebmeier <ethangrebmeier@gmail.com>
2021-06-22 18:09:10 -0700

    Gave the snackbar a z-index

--- components/Snackbar/Snackbar.js
+++ components/Snackbar/Snackbar.js
@@ -5,6 +5,7 @@ import IconButton from "../Buttons/IconButton";
 
 const Wrapper = styled.div`
     position: absolute;
+    z-index: 100;
     bottom: ${props => props.isShowing ? '10px' : '-70px'};
     left: 50%;
     transform: translateX(-50%);
